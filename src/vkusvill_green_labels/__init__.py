53df9a86143ca2c88974b553f61f6e5d3caa77da
Aidar Garikhanov <a1d4r@yandex.ru>
2023-09-13 00:23:09 +0300

    Initial commit

---
+++ src/vkusvill_green_labels/__init__.py
@@ -0,0 +1,1 @@
+"""vkusvill-green-labels package."""
---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+"""Test vkusvill-green-labels."""
+
+import vkusvill_green_labels
+
+
+def test_import() -> None:
+    """Test that the package can be imported."""
+    assert isinstance(vkusvill_green_labels.__name__, str)
