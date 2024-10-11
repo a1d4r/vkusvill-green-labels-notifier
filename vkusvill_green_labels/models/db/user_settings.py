03047809e58a5d7944ea9beeaec97e679efe4c7c
Aidar Garikhanov <a1d4r@yandex.ru>
2024-10-12 00:00:31 +0300

    Remove old address fields in user settings

--- vkusvill_green_labels/models/db/user_settings.py
+++ vkusvill_green_labels/models/db/user_settings.py
@@ -1,12 +1,12 @@
 from uuid import uuid4
 
-from sqlalchemy import UUID, Numeric
+from sqlalchemy import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from vkusvill_green_labels.models.db.address import Location
 from vkusvill_green_labels.models.db.base import Base
 from vkusvill_green_labels.models.db.utils.pydantic_type import PydanticType
-from vkusvill_green_labels.models.types import Latitude, Longitude, UserSettingsID
+from vkusvill_green_labels.models.types import UserSettingsID
 from vkusvill_green_labels.models.vkusvill import VkusvillUserSettings
 
 
@@ -14,9 +14,6 @@ class UserSettings(Base):
     __tablename__ = "user_settings"
 
     id: Mapped[UserSettingsID] = mapped_column(UUID, default=uuid4, primary_key=True)
-    address_latitude: Mapped[Latitude | None] = mapped_column(Numeric, nullable=True)
-    address_longitude: Mapped[Longitude | None] = mapped_column(Numeric, nullable=True)
-    address: Mapped[str | None]
     enable_notifications: Mapped[bool] = mapped_column(default=True)
     vkusvill_settings: Mapped[VkusvillUserSettings | None] = mapped_column(
         PydanticType(VkusvillUserSettings)
