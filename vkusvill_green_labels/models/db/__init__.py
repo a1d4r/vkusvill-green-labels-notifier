f7dda2edcab0a681d26a074aea356d09e06f1222
Aidar Garikhanov <a1d4r@yandex.ru>
2024-10-12 00:19:00 +0300

    Sort imports

--- vkusvill_green_labels/models/db/__init__.py
+++ vkusvill_green_labels/models/db/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["Base", "User", "UserSettings", "Location"]
 
-from vkusvill_green_labels.models.db.address import Location  # noqa: I001
+from vkusvill_green_labels.models.db.address import Location
 from vkusvill_green_labels.models.db.base import Base
-from vkusvill_green_labels.models.db.user_settings import UserSettings
 from vkusvill_green_labels.models.db.user import User
+from vkusvill_green_labels.models.db.user_settings import UserSettings
