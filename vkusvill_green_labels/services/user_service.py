8b381509053bfc366a580068b6b41f9f3e14c537
Aidar Garikhanov <a1d4r@yandex.ru>
2024-10-12 00:17:50 +0300

    Fix bug when user was missing in database and settings were not saved

--- vkusvill_green_labels/services/user_service.py
+++ vkusvill_green_labels/services/user_service.py
@@ -13,26 +13,34 @@ from vkusvill_green_labels.repositories.user import UserRepository
 class UserService:
     user_repository: UserRepository
 
-    async def get_user_location(self, telegram_user: TelegramUser) -> Location | None:
+    async def get_or_create_user(self, telegram_user: TelegramUser) -> User:
         user = await self.user_repository.get_user_by_telegram_id(telegram_user.id)
         if not user:
-            return None
+            user = User(
+                tg_id=telegram_user.id,
+                first_name=telegram_user.first_name,
+                last_name=telegram_user.last_name,
+                username=telegram_user.username,
+                settings=UserSettings(locations=[]),
+            )
+            await self.user_repository.add_user(user)
+            logger.info("Created new user: {}", user.tg_id)
+        return user
+
+    async def get_user_location(self, telegram_user: TelegramUser) -> Location | None:
+        user = await self.get_or_create_user(telegram_user)
         if not user.settings.locations:
             return None
         return user.settings.locations[0]
 
     async def get_user_notifications_settings(self, telegram_user: TelegramUser) -> bool:
-        user = await self.user_repository.get_user_by_telegram_id(telegram_user.id)
-        if not user:
-            return False
+        user = await self.get_or_create_user(telegram_user)
         return user.settings.enable_notifications
 
     async def update_user_notifications_settings(
         self, telegram_user: TelegramUser, enable: bool
     ) -> None:
-        user = await self.user_repository.get_user_by_telegram_id(telegram_user.id)
-        if not user:
-            return
+        user = await self.get_or_create_user(telegram_user)
         user.settings.enable_notifications = enable
         await self.user_repository.update_user(user)
 
@@ -40,37 +48,16 @@ class UserService:
         self, telegram_user: TelegramUser, address_info: AddressInfo
     ) -> None:
         logger.info("Saving location for user {}: {}", telegram_user.id, address_info)
-        user = await self.user_repository.get_user_by_telegram_id(telegram_user.id)
-        if not user:
-            logger.info("Creating new user for telegram_id: {}", telegram_user.id)
-            user = User(
-                tg_id=telegram_user.id,
-                first_name=telegram_user.first_name,
-                last_name=telegram_user.last_name,
-                username=telegram_user.username,
-                settings=UserSettings(
-                    locations=[
-                        Location(
-                            latitude=address_info.latitude,
-                            longitude=address_info.longitude,
-                            address=address_info.address,
-                        )
-                    ]
-                ),
+        user = await self.get_or_create_user(telegram_user)
+        user.settings.locations = [
+            Location(
+                latitude=address_info.latitude,
+                longitude=address_info.longitude,
+                address=address_info.address,
             )
-            await self.user_repository.add_user(user)
-            logger.info("Created new user for telegram_id: {}", telegram_user.id)
-        else:
-            user.settings.locations = [
-                Location(
-                    latitude=address_info.latitude,
-                    longitude=address_info.longitude,
-                    address=address_info.address,
-                )
-            ]
-            # After changing address we should create Vkusvill account and set new address
-            user.settings.vkusvill_settings = None
-            flag_modified(user.settings, "vkusvill_settings")
-            await self.user_repository.update_user(user)
-            logger.info("Updated address for user {}", telegram_user.id)
+        ]
+        # After changing address we should create new Vkusvill account and set new address
+        user.settings.vkusvill_settings = None
+        flag_modified(user.settings, "vkusvill_settings")
+        await self.user_repository.update_user(user)
         logger.info("Saved location for user {}: {}", telegram_user.id, user.settings.locations[0])
